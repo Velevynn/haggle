950502fe15bd501f928f587e2866e908a60e62cb
jestra03 <joshemestra@gmail.com>
2024-02-20 23:15:06 -0800

    demo finish

---
+++ react-frontend/src/pages/About.js
@@ -0,0 +1,16 @@
+// About.js (Josh)
+import React, { useState, useEffect } from "react";
+import './pages.css';
+
+function About() {
+    
+  return (
+    <div style={{margin: '25px', textAlign: 'center'}}>
+      <h style={{ fontFamily: 'Newsreader, serif', fontSize: '3rem'}}>About</h>
+      <div className="divider" />
+    </div>
+    
+  );
+}
+
+export default About;
