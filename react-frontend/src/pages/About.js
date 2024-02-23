b2ac6ebc58f5f224b995f42980cc84f71db35fe5
jestra03 <joshemestra@gmail.com>
2024-02-23 12:38:08 -0800

    added bio page, bio entry component

--- react-frontend/src/pages/About.js
+++ react-frontend/src/pages/About.js
@@ -1,6 +1,7 @@
 // About.js (Josh)
 import React, { useState, useEffect } from "react";
 import './pages.css';
+import Bio from '../components/Bio';
 
 function About() {
     
@@ -8,6 +9,28 @@ function About() {
     <div style={{margin: '25px', textAlign: 'center'}}>
       <h style={{ fontFamily: 'Newsreader, serif', fontSize: '3rem'}}>About</h>
       <div className="divider" />
+      <div className= "team-container">
+        <Bio name = "Alex" bio = 
+        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. 
+        Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."/>
+
+        <Bio name = "Alexander" bio = 
+        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. 
+        Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."/>
+
+        <Bio name = "Joshua" bio = 
+        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. 
+        Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."/>
+
+<       Bio name = "Jimmy" bio = 
+        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. 
+        Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."/>
+
+        <Bio name = "Karan" bio = 
+        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. 
+        Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."/>
+      </div>
+
     </div>
     
   );
