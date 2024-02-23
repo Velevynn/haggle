80796164eb5ad2dd2ebf999534815ef4dac0ccac
jestra03 <joshemestra@gmail.com>
2024-02-23 12:38:14 -0800

    added bio page, bio entry component

---
+++ react-frontend/src/components/Bio.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./bio.css";
+import defaultPfp from "../resources/default-pfp.jpg"
+
+function BioEntry(props) {
+    const {name, photoPath, bio} = props;
+
+
+    return (
+        <div className = "bio-entry">
+            <label className = "name">
+                {name}
+            </label>
+            <div className = "bio-container" style = {{backgroundImage: `url(${defaultPfp})`}}>
+                <div className="bio-text">
+                    {bio}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default BioEntry;
